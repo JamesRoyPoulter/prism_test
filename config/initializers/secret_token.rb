d27b184d781033d518bcd7c33ed81ce59abf5ad1
JRP <jamesroypoulter@gmail.com>
2013-10-02 11:10:27 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Prism::Application.config.secret_token = '6c9b87889e2b55039598f4b00b8cdd132f9a5b670958dab3bda99c84c030c97895acf8c2a5e67e72fad67e5360e5368771d7fc1e1837451b99cb3ebcb0c2baa1'
